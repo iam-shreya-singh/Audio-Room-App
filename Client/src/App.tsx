3d328fedc077b0dc3a0eb36523dcfef0e04540c5
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-13 11:45:22 +0000

    c

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,7 +1,15 @@
-import './App.css'
+import "./App.css";
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom' 
+import { MainPage } from "./pages/main";
 
 function App() {
 return 
-    <></>;
-}
+    <Router> 
+     <Routes>
+        <Route path="/" element={<MainPage />}/>
+     </Routes>
+    </Router>;
+   };
+   
 export default App;
