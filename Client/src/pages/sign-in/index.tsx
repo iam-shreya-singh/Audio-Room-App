e7e5b83d085ecdad33e7659bf981b3f92a689f46
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-14 13:48:49 +0000

    commit

--- Client/src/pages/sign-in/index.tsx
+++ Client/src/pages/sign-in/index.tsx
@@ -1,9 +1,19 @@
 export const SignIn = () => {
-    return 
+    return (
     <div className="sign-in"> 
-    {""}
-    <h1>
-    Welcome to Audio Room 
-    </h1>
-    </div>;
+    {" "}
+    <h1> Welcome to AudioRoom Chats ... Connect, Share, and Amplify Your Voice. </h1>
+    <form>
+   <div>
+    <label> Username: </label>
+    <input type="text" />
+   </div>
+   <div>
+    <label> Name: </label>
+    <input type="text" />
+   </div>
+     <button type="submit"> Sign In </button>
+    </form>
+    </div>
+    );
 };
