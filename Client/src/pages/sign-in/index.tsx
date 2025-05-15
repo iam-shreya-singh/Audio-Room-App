a03d90ccac3caf73b891d3067e1f3c4915800831
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-15 10:16:22 +0000

    commit

--- Client/src/pages/sign-in/index.tsx
+++ Client/src/pages/sign-in/index.tsx
@@ -3,20 +3,20 @@ import { yupResolver } from "@hookform/resolvers/yup";
 import * as yup from "yup";
 
 interface FormValues{
-  Username: string;
+  username: string;
   name: string;
 }
 
 export const SignIn = () => {
     // Define Yup validation schema
     const schema = yup.object().shape ({    
-      username: yup.string().required("Username is required").matches(/^[a-zA-Z0-9_]{3,15}$/, "Username must be 3-15 characters long and contain only letters, numbers, and underscores"),
+      username: yup.string().required("username is required").matches(/^[a-zA-Z0-9_]{3,15}$/, "username must be 3-15 characters long and contain only letters, numbers, and underscores"),
       name: yup.string().required("Name is required"),
     });
 
     const onSubmit : (SubmitHandler<FormValues>) = (data) => {
-      const {Username, name} = data;
-      console.log(Username, name);
+      const {username, name} = data;
+      console.log(username, name);
     }; 
 
     const {
@@ -31,14 +31,13 @@ export const SignIn = () => {
     <h1> Welcome to AudioRoom Chats ... Connect, Share, and Amplify Your Voice. </h1>
     <form>
    <div onSubmit={handleSubmit(onSubmit)}>
-    <label> Username: </label>
-    <input type="text" {...register("Username")} />
-    {errors.Username && (<p style = {{color: "red" }} > 
-    errors.Username.message </p>)}
+    <label> username: </label>
+    <input type="text" {...register("username")} />
+    {errors.username && (<p style = {{color: "red" }} > errors.username.message </p>)}
    </div>
    <div>
     <label> Name: </label>
-    <input type="text" {...register("Username")}/>
+    <input type="text" {...register("username")}/>
    </div>
      <button type="submit"> Sign In </button>
     </form>
