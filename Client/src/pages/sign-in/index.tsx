b736c31918f8d54580524a3014fd119ad3a8c472
Shreya Singh <shreyasinghin24@gmail.com>
2025-05-18 11:09:37 +0000

    yup validation changes

--- Client/src/pages/sign-in/index.tsx
+++ Client/src/pages/sign-in/index.tsx
@@ -10,9 +10,10 @@ interface FormValues{
 export const SignIn = () => {
     // Define Yup validation schema
     const schema = yup.object().shape ({    
-      username: yup.string().required("username is required").matches(/^[a-zA-Z0-9_]{3,15}$/, "username must be 3-15 characters long and contain only letters, numbers, and underscores"),
-      name: yup.string().required("Name is required"),
-    });
+      username: yup.string().required("username is required").matches(/^[a-zA-Z0-9_.@$]+$/, "Invalid username")
+       .required("Username is required"),
+    name: yup.string().required("Name is required"),
+  });
 
     const onSubmit : (SubmitHandler<FormValues>) = (data, event) => {
       event?.preventDefault();
