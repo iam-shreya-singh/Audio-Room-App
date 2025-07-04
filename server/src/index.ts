d6957eef3da13d01da3a0470dad93f1515eb0f7a
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-04 13:45:30 +0000

    nodemon crash

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,16 +1,18 @@
 import express from "express";
+// import bodyParser from "body-parser";
 import cors from "cors";
+// import dotenv from "dotenv";
 
-import authRoute from "./routes/auth";
+import authRoutes from "./auth";
 
 const app = express();
+
 app.use(express.json());
 app.use(cors());
 
-app.use("/auth", authRoute);
+app.use("/auth", authRoutes);
 
-const PORT = process.env.PORT || 3001;  //backend port
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-    });
-
+  console.log(`Server is running on port ${PORT}`);
+});
--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -4,13 +4,13 @@ import { UserObjectRequest } from "@stream-io/node-sdk";
 
 const router = Router();
 
-router.post("/createUser", async (req: Request, res: Response) =>{
-    const {username, name, image} = req.body;
+router.post("/createUser", async (req: Request, res: Response) => {
+    const { username, name, image } = req.body;
     // Validate input
     if (!username || !name || !image) {
         return res.status(400).json({error: "All fields are required"});
     }
-    const newUser: UserObjectReuest = {
+    const newUser: UserObjectRequest = {
         id: username,
         role: "user",
         name,
@@ -22,6 +22,10 @@ router.post("/createUser", async (req: Request, res: Response) =>{
             [newUser.id]: newUser,
             },
         });
+
+        const expiry = Math.floor(Date.now()/ 1000) + 24 * 60 * 60;
+        const token = Client.createToken(username, expiry);
+        return res.status(200).json({ token, username, name});
 });
 
 export default router;
