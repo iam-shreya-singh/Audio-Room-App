13f90f38a3148c442cb3294570ef16e6aa98b695
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-18 18:36:44 +0000

    express update, restart node mods

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,28 +1,22 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
+import { Request, Response } from "express";
 import { Client } from "../stream-Client";
-import { UserObjectRequest } from "@stream-io/node-sdk";
 
 const router = Router();
-
 router.post("/createUser", async (req: Request, res: Response) => {
     const { username, name, image } = req.body;
     // Validate input
     if (!username || !name || !image) {
         return res.status(400).json({error: "All fields are required"});
     }
-    const newUser: UserObjectRequest = {
+    const newUser = {
         id: username,
         role: "user",
         name,
         image,
         
     }
-        const user = await Client.upsertUsers({
-            users: {
-            [newUser.id]: newUser,
-            },
-        });
-
+        const user = await Client.upsertUsers([ newUser ]);
         const expiry = Math.floor(Date.now()/ 1000) + 24 * 60 * 60;
         const token = Client.createToken(username, expiry);
         return res.status(200).json({ token, username, name});
