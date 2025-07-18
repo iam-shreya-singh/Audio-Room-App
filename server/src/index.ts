013efd860a8d9e1e1f403143e5946d5a80440b7e
Shreya Singh <shreyasinghin24@gmail.com>
2025-07-18 16:58:57 +0000

    c

--- server/src/index.ts
+++ server/src/index.ts
@@ -3,7 +3,7 @@ import express from "express";
 import cors from "cors";
 // import dotenv from "dotenv";
 
-import authRoutes from "./auth";
+import authRoutes from "./routes/auth"; 
 
 const app = express();
 
